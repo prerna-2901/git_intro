ec280a3ed4e41c8db2366920f60cdde62111ebc7
Prerna <prernasrivastav03@gmail.com>
2017-10-02 15:14:10 +0530

    sheldon greeter

--- file01.js
+++ file01.js
@@ -1,4 +1,6 @@
 function greet(name){
-console.log('hello world' + name)
+console.log('knock knock knock' + name)
+console.log('knock knock knock' + name)
+console.log('knock knock knock' + name)
 }
 greet('Prerna')
